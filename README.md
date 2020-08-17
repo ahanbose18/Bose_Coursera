3da213ca440b0846b051e198edc25edceeb0f277
ahan bose <ahan.bose@gmail.com>
2020-08-17 23:16:15 +0530

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-/* Add Author and Project Details here */
+### Author Name: Ahan Bose
+## Project Description
+ Program to find statistic analysis of character array of length 40. Finding mean, median, maximum, minimum, arranging in ascending order and to print the array itself.
+## Coded in C gcc compiler. 
--- stats.c
+++ stats.c
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.c> 
+ * @brief <contains main and function definitions >
  *
- * <Add Extended Description Here>
+ * <program to find statisitcal analysis of unsigned array of characters>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Ahan Bose>
+ * @date <17-08-2020>
  *
  */
 
@@ -27,6 +27,9 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
+
+
+
 void main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
@@ -36,8 +39,168 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
+  unsigned char *ptr; /* pointer pointing to array*/
+  unsigned int length = SIZE;
+  ptr = test;
   /* Statistics and Printing Functions Go Here */
+  print_array(ptr,length);
+  print_statistics(ptr,length);
+  
 
 }
 
 /* Add other Implementation File Code Here */
+void print_statistics(unsigned char* ptr,unsigned int length)
+ {
+  unsigned char median;
+  unsigned char mean;
+  unsigned char maximum;
+  unsigned char minimum;
+  unsigned char *reversed_array;
+  median = find_median(ptr,length);
+  printf("__Median is__ %d \n",median);
+  mean = find_mean(ptr,length);
+  printf("__Mean is__ %d \n",mean);
+  maximum = find_maximum(ptr,length);
+  printf("__Maximum is__%d \n",maximum);
+  minimum = find_minimum(ptr,length);
+  printf("__Minimum is__%d \n",minimum);
+  reversed_array = sort_array(ptr,length);
+  printf("__Sorted Array__ \n");
+  print_array(reversed_array,length);
+  
+ }
+ 
+ 
+ 
+ 
+void print_array(unsigned char* ptr,unsigned int length)
+ {
+ 
+   printf("__The elements of the array are__ \n");
+   for(int i=0;i<length;i++)
+    {
+        
+    	printf("%d \n",*(ptr+i));
+  
+ }
+} 
+ 
+unsigned char find_median(unsigned char* ptr,unsigned int length)
+ {
+ 	
+   unsigned char result; 	
+   int i=0 , j=0 , temp=0;
+
+   for(i=0 ; i<length ; i++)
+    {
+        for(j=0 ; j<length-1 ; j++)
+        {
+            if(ptr[j]>ptr[j+1])
+            {
+                temp        = ptr[j];
+                ptr[j]    = ptr[j+1];
+                ptr[j+1]  = temp;
+            }
+        }
+    }
+ 	
+ 
+ 	
+ 	if(length%2==0)
+ 	     result = (ptr[(length-1)/2] + ptr[length/2])/2.0;
+       else
+            result = ptr[length/2];
+ 	   
+   return result;
+ }
+ 
+ 
+unsigned char find_mean(unsigned char* ptr,unsigned int length)
+ {
+ 	
+   unsigned char result; 	
+   int i=0;
+   int s=0;
+
+   for(i=0 ; i<length ; i++)
+    {
+       s+=*ptr;
+       ptr++; 
+    }
+    
+    result = s/length;
+    return result;
+
+ }
+ 
+ 
+ unsigned char find_maximum(unsigned char* ptr,unsigned int length)
+  {
+   char max=0;
+   char max_index=0;
+   for(int i=0;i<length;i++)
+    {
+     if(ptr[max_index]<ptr[i])
+      {
+       max_index=i;
+      }
+    }
+   max = ptr[max_index];
+   return max; 
+    
+  }
+ 
+ 
+ unsigned char find_minimum(unsigned char* ptr,unsigned int length)
+  {
+   char min=0;
+   char min_index=0;
+   for(int i=0;i<length;i++)
+    {
+     if(ptr[min_index]>ptr[i])
+      {
+       min_index=i;
+      }
+    }
+   min = ptr[min_index];
+   return min; 
+    
+  }
+ 
+ unsigned char* sort_array(unsigned char* ptr,unsigned int length)
+  {
+	    
+	  int i=0 , j=0 , temp=0;
+
+	   for(i=0 ; i<length ; i++)
+	    {
+		for(j=0 ; j<length-1 ; j++)
+		{
+		    if(ptr[j]<ptr[j+1])
+		    {
+		        temp        = ptr[j];
+		        ptr[j]    = ptr[j+1];
+		        ptr[j+1]  = temp;
+		    }
+		}
+	    }
+	 return ptr; 
+  
+  }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
