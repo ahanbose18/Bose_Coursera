0c9b4e0fcb7cdcdc2f8b710ee4625c2abb07de95
ahan bose <ahan.bose@gmail.com>
2020-08-17 20:14:17 +0530

    Adding code outline and documentation for c1m1

--- stats.h
+++ stats.h
@@ -22,19 +22,97 @@
 #define __STATS_H__
 
 /* Add Your Declarations and Function Comments here */ 
-
+void print_statistics(unsigned char*,unsigned int);
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief finds statistics based on unsigned character array of data
  *
- * <Add Extended Description Here>
+ * <A function that prints the statistics of an array including minimum, maximum, mean, and median.>
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param <pointer> <A unsigned char pointer to an n-element data array>
+ * @param <length> <An unsigned integer as the size of the array>
  *
- * @return <Add Return Informaiton here>
+ * @return void
  */
+ 
+ /* Add Your Declarations and Function Comments here */ 
+void print_array(unsigned char*,unsigned int);
+/**
+ * @brief <A function that prints the statistics of an array including minimum, maximum, mean, and median.>
+ *
+ * <A function that prints the statistics of an array including minimum, maximum, mean, and median.>
+ *
+ * @param <pointer> <A unsigned char pointer to an n-element data array>
+ * @param <length> <An unsigned integer as the size of the array>
+ *
+ * @return void
+ */
+
+
+/* Add Your Declarations and Function Comments here */ 
+unsigned char find_median(unsigned char*,unsigned int);
+/**
+ * @brief <finds median>
+ *
+ * <Given an array of data and a length, returns the median value>
+ *
+* @param <pointer> <A unsigned char pointer to an n-element data array>
+* @param <length> <An unsigned integer as the size of the array>
+ *
+ * @return <unsigned char median>
+ */
+
+
+/* Add Your Declarations and Function Comments here */ 
+unsigned char find_mean(unsigned char*,unsigned int);
+/**
+ * @brief <finds mean>
+ *
+ * <Given an array of data and a length, returns the median value>
+ *
+ * @param <pointer> <A unsigned char pointer to an n-element data array>
+ * @param <length> <An unsigned integer as the size of the array>
+ *
+ * @return <unsigned char mean>
+ */
+
+/* Add Your Declarations and Function Comments here */ 
+unsigned char find_maximum(unsigned char*,unsigned int);
+/**
+ * @brief <finds maximum>
+ *
+ * <Given an array of data and a length, returns the maximum value>
+ *
+ * @param <pointer> <A unsigned char pointer to an n-element data array>
+ * @param <length> <An unsigned integer as the size of the array>
+ *
+ * @return <unsigned char maximum>
+ */
+ 
+ unsigned char find_minimum(unsigned char*,unsigned int);
+/**
+ * @brief <finds minimum>
+ *
+ * <Given an array of data and a length, returns the minimum value>
+ *
+ * @param <pointer> <A unsigned char pointer to an n-element data array>
+ * @param <length> <An unsigned integer as the size of the array>
+ *
+ * @return <unsigned char minimum>
+ */
+
+unsigned char* sort_array(unsigned char*,unsigned int);
+/**
+ * @brief <sorts in descending order>
+ *
+ * <Given an array of data and a length, sorts the array from largest to smallest. (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+>
+ *
+ * @param <pointer> <A unsigned char pointer to an n-element data array>
+ * @param <length> <An unsigned integer as the size of the array>
+ *
+ * @return <pointer to unsigned char sorted array >
+ */
+
 
 
 #endif /* __STATS_H__ */
